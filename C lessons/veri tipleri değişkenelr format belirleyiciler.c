2b8484ab5981e6800f14d61f00df14c99ab51e2b
kagansenkeser <98644514+kagansenkeser@users.noreply.github.com>
2022-03-02 02:09:00 +0300

    Create veri tipleri değişkenelr format belirleyiciler.c

---
+++ C lessons/veri tipleri değişkenelr format belirleyiciler.c
@@ -0,0 +1,19 @@
+Temel değişkenler    
+değişkenler değerleri depolamak için kullandığımız değer tutuculardır 
+int          tamsayi
+float        ondalikli sayi
+double       ondalikli sayi ama daha fazla değer alabiliyor
+char         kelimeler için
+
+
+format belirleyiciler
+format belirleyiciler  bilgisayara kullandigimiz degerin ne oldugunu haber vermemizi sagliyor örneğin 5 dediğimizde bu bir sayi mi yada kelime mi olduğunu bilgisayarın anlamsını sağlar
+
+int için   %d
+örnegin
+int a =5;
+printf("%d",a);
+basta a ya 5 degerini atıyoruz ve sonra printf de bir sayi degerini yazdircagimizi soyleyip ardindan hangi degeri yazdiracagimizi söylüyoruz
+float için %f
+double için %f
+char için  %c
