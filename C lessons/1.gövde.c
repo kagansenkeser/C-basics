f3a9487e4ad8cb7b09057a973ade44105a289ec6
kagansenkeser <98644514+kagansenkeser@users.noreply.github.com>
2022-03-02 01:56:10 +0300

    Create 1.gövde.c

---
+++ C lessons/1.gövde.c
@@ -0,0 +1,12 @@
+// altaki kisim bizim gövdemiz çalışmasını istediğimiz her şeyi 7. satırdan başlayıp 10. satıra kadar yazıyoruz entere basarak aralığı genişletebiliiz anack int main(){ ve return0;  arasında olmak zorunda
+
+
+
+#include<stdio.h>
+int main()
+{
+	
+	
+	
+	return 0;
+}
