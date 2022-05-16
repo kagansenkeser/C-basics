7d3bc174024cf761b7e5c9d2017d0f26507449e1
Kağan Şenkeser <98644514+kagansenkeser@users.noreply.github.com>
2022-05-16 16:02:21 +0300

    Update 7.while dongusu.c

--- C lessons/7.while dongusu.c
+++ C lessons/7.while dongusu.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+ 
+int main () {
 
+   // oncelikle bir deger tanimliyoruz dongu icin
+   int a = 10;
+
+   // while diyip parantez icine sartimizi yaziyoruz
+   while( a < 20 ) {
+   	//sartimiz saglandigi sure boyunca kod calisicak
+   	//
+      printf("a'nin degeri : %d\n", a);
+      a++;
+    // a degerini arttiriyoruz ki dongumuz sonsuza dek surmesin
+   }
+ 
+   return 0;
+}
