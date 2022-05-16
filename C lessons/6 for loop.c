86a5ec20470b3ae173cfca07823749c5fc57656a
kagansenkeser <98644514+kagansenkeser@users.noreply.github.com>
2022-05-16 15:53:11 +0300

    Update 6 for loop.c

--- C lessons/6 for loop.c
+++ C lessons/6 for loop.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 int main(){
-	//how use for 
+	//for nası kullanılır
 	for(int i=1;i<5;i++)
+	// ilk olarak bir i değişkeni oluşuruyoruz döngümüz buna bağlıdır  ;  döngünün çalışma şartını söylüyoruz burda i 5 den kücük ise  ;
+		#sonrasinda degiskenin ne kadar articagini soyluyoruz
 		{		
 		printf("%d. defa tur\n",i);
+		# 4 defa calisicak bunun sebebi 5 den kucuk ise dememiz 1 icin 2 icin 3 icin ve 4 icin  5 e esit oldugunda calismayacaktir
+		
 		}
 return 0;
 }
